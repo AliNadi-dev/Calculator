c5d8950b28b07686157679393703005e4eecc0dc
Ali Nadi <a88nadi@gmail.com>
2024-09-30 10:13:35 +0200

    functions added to the mainApp

---
+++ mainApp.py
@@ -0,0 +1,10 @@
+import devision
+import multiply
+import sum
+
+a = int(input("enter first number"))
+b = int(input("enter first number"))
+
+print( devision(a,b))
+print(multiply(a,b))
+print(sum(a,b))
