8c510fee5810745f45e56da00abd55509bf314a4
Ali Nadi <a88nadi@gmail.com>
2024-09-30 09:57:08 +0200

    proper message is defiend in the function

--- devision.py
+++ devision.py
@@ -3,6 +3,7 @@ def devide(a,b):
     try:
         c=a/b
     except:
-        print('invalid input')
+        print('invalid input. zero value for denominator!')
+        print('enter new inputs')
         
     return c
